93399175abaf1ed3b8b8dfcdf3c7cdd1e7e80d62
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-08-05 16:28:37 +0800

    update logging to show filename and line

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 var mappingsDir string = "./mappings"
 
 func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	
 	mockServer := service.NewMockServer("/mock", mappingsDir)
 	if err := mockServer.RegisterHandlers(); err != nil {
 		log.Printf("error loading mappings: %s\n", err.Error())

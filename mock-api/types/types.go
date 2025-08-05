b654115be9b94f693962f3b5842ce5f1dfd7b57a
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-08-05 13:20:06 +0800

    add mapping type

--- mock-api/types/types.go
+++ mock-api/types/types.go
@@ -1,6 +1,14 @@
 package types
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
+
+type Mapping struct {
+	Path     string   `json:"path"`
+	Method   string   `json:"method"`
+	Response Response `json:"response"`
+}
 
 type Response struct {
 	StatusCode int               `json:"statusCode"`
